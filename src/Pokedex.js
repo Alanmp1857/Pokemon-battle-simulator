2d8513cd2fbdb33ae21073d217daaf2aa3c7e8d1
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2022-12-06 10:33:45 +0530

    Second commit

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-// import TablePagination from "@mui/material/TablePagination";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import charmander from "./images/charmander.png";
+import rows from "./Data.js";
 
 const columns = [
   {
@@ -76,26 +76,11 @@ const columns = [
   },
 ];
 
-const rows = [
-  [
-    charmander,
-    "Charmander",
-    "Fire",
-    "Blaze | Solarpower",
-    "39",
-    "52",
-    "43",
-    "60",
-    "50",
-    "65",
-    "309",
-  ],
-];
-
-function Pokedex() {
+function Pokedex(props) {
+  console.log(props);
   return (
     <div className="h-1/2">
-      <Paper sx={{ width: "70%", overflow: "hidden" }}>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
         <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
@@ -114,15 +99,20 @@ function Pokedex() {
             <TableBody>
               {rows.map((row, index) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={index}
+                    onClick={() =>
+                      props.sPokemon((oldarr) => [...oldarr, row[1]])
+                    }
+                  >
                     {columns.map((ele, index) => {
                       return (
                         <TableCell key={index} align={ele.align}>
-                          {ele.id === 0 ? (
-                            <img
-                              className="h-14 cover scale-[2.5]"
-                              src={row[0]}
-                            ></img>
+                          {ele.id == 0 ? (
+                            <img className="h-8 scale-[2.5]" src={row[0]}></img>
                           ) : (
                             row[ele.id]
                           )}
